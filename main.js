f3f8efe8be608b5f223a56be3244580087e1baf3
Istvan Fazekas <fazekas.istvan.90@gmail.com>
2018-07-31 11:37:27 +0200

    create first implementation of the blockchain

--- main.js
+++ main.js
@@ -9,7 +9,27 @@ class Block {
         this.hash = this.calculateHash();
     }
 
-    calculateHash(){
+    calculateHash() {
         return SHA256((this.index * 23).toString() + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class BlockChain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "31/07/2018", "Genesis Block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock.hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
     }
 }
