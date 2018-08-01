77a8461501d0848898ae93ffd124818519a17d6e
Istvan Fazekas <fazekas.istvan.90@gmail.com>
2018-08-01 09:47:41 +0200

    implement basic PoW logic with nonce and difficulty level

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const SHA256 = require('crypto-js/sha256');
 
 class Block {
@@ -7,16 +9,27 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256((this.index * 23).toString() + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256((this.index * 23).toString() + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class BlockChain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
     }
 
     createGenesisBlock() {
@@ -29,7 +42,7 @@ class BlockChain {
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -52,8 +65,11 @@ class BlockChain {
 }
 
 let beantrail = new BlockChain();
+
+console.log("Mining block 1...");
 beantrail.addBlock(new Block(1, "10/08/2018", {farmName: "Piri Ungo", currentLocation: "Africa"}));
+
+console.log("Mining block 2...");
 beantrail.addBlock(new Block(2, "20/08/2018", {farmName: "Piri Ungo", currentLocation: "Morocco"}));
-beantrail.addBlock(new Block(3, "23/08/2018", {farmName: "Piri Ungo", currentLocation: "The Netherlands"}));
 
 console.log(JSON.stringify(beantrail, null, 4));
